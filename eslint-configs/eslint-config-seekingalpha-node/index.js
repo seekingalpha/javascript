d99bd4abb6af719331e4837c49df6c84d0a58d1c
Artem Prokhatskyi <artem.prokhatskyi@seekingalpha.com>
2018-09-17 13:51:10 +0300

    [MW-1301] added rules for node

--- eslint-configs/eslint-config-seekingalpha-node/index.js
+++ eslint-configs/eslint-config-seekingalpha-node/index.js
@@ -3,11 +3,15 @@ module.exports = {
   parser: 'babel-eslint',
 
   extends: [
+    'plugin:node/recommended',
+
     // https://www.npmjs.com/package/eslint-config-seekingalpha-base
     'seekingalpha-base',
 
+    './rules/eslint/index.js',
+
     // https://github.com/mysticatea/eslint-plugin-node
-    './rules/eslint-plugin-node/index.js',
+    './rules/eslint-plugin-node/index.js'
   ],
 
   env: {
