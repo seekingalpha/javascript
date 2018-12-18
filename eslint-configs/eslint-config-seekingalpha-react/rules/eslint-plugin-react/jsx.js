b1478da671f3dfc35fc23445f1b411888cdbde01
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2018-12-18 17:36:59 +0200

    eslint-config-seekingalpha-react v. 1.4.0
    - [new] `react/no-danger` rule error

--- eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react/jsx.js
+++ eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react/jsx.js
@@ -218,7 +218,7 @@ module.exports = {
     ],
 
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
-    'react/no-danger': 'off',
+    'react/no-danger': 'error',
 
   },
 
