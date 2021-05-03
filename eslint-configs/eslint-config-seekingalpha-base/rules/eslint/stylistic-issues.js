a72fd031035f97a8988ca07d16ec8308ea264c54
Aleksey Kovalevsky <aleksey.kovalevsky@gmail.com>
2021-05-03 14:00:21 +0300

    eslint-config-seekingalpha-base ver. 4.42.0 (#113)
    - [patch] update `comma-dangle` rule

--- eslint-configs/eslint-config-seekingalpha-base/rules/eslint/stylistic-issues.js
+++ eslint-configs/eslint-config-seekingalpha-base/rules/eslint/stylistic-issues.js
@@ -68,8 +68,13 @@ module.exports = {
 
     // https://eslint.org/docs/rules/comma-dangle
     'comma-dangle': [
-      'error',
-      'always-multiline',
+      'error', {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
     ],
 
     // https://eslint.org/docs/rules/comma-spacing
