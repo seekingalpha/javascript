bf83eb05e836b1057e1d5353e086234eaabd3a07
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2022-01-17 13:27:47 +0200

    eslint-config-seekingalpha-tests ver. 0.2.0 (#167)
    - [deps] integrate `eslint-plugin-testing-library`

---
+++ eslint-configs/eslint-config-seekingalpha-tests/rules/eslint-plugin-testing-library/index.js
@@ -0,0 +1,69 @@
+module.exports = {
+
+  plugins: ['testing-library'],
+
+  rules: {
+    // Testing Library Plugin https://github.com/testing-library/eslint-plugin-testing-library
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/await-async-query.md
+    'testing-library/await-async-query': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/await-async-utils.md
+    'testing-library/await-async-utils': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-await-sync-query.md
+    'testing-library/no-await-sync-query': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-container.md
+    'testing-library/no-container': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-debugging-utils.md
+    'testing-library/no-debugging-utils': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-dom-import.md
+    'testing-library/no-dom-import': [
+      'error',
+      'react',
+    ],
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-node-access.md
+    'testing-library/no-node-access': 'off',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-promise-in-fire-event.md
+    'testing-library/no-promise-in-fire-event': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-render-in-setup.md
+    'testing-library/no-render-in-setup': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-unnecessary-act.md
+    'testing-library/no-unnecessary-act': 'off',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-wait-for-empty-callback.md
+    'testing-library/no-wait-for-empty-callback': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-wait-for-multiple-assertions.md
+    'testing-library/no-wait-for-multiple-assertions': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-wait-for-side-effects.md
+    'testing-library/no-wait-for-side-effects': 'off',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-wait-for-snapshot.md
+    'testing-library/no-wait-for-snapshot': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-find-by.md
+    'testing-library/prefer-find-by': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-presence-queries.md
+    'testing-library/prefer-presence-queries': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-query-by-disappearance.md
+    'testing-library/prefer-query-by-disappearance': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-screen-queries.md
+    'testing-library/prefer-screen-queries': 'error',
+
+    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/render-result-naming-convention.md
+    'testing-library/render-result-naming-convention': 'error',
+  },
+
+};
