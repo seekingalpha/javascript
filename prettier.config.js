3106c8aad19490672b8b0e8012b64ffc5448d4b9
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2023-07-06 12:03:04 +0300

    seekingalpha-javascript-style ver. 5.9.0

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,7 @@
-const config = require('@seekingalpha/prettier-config-samw');
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+import config from '@seekingalpha/prettier-config-samw';
 
-module.exports = {
+// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export, node/no-unsupported-features/es-syntax
+export default {
   ...config,
 };
