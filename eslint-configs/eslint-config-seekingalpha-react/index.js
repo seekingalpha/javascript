76eac4096ead73ed87a848285b34d5f708840789
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2023-07-25 11:31:34 +0300

    eslint-config-seekingalpha-react ver. 7.10.0
    - [breaking] drop `class-methods-use-this` extension rule

--- eslint-configs/eslint-config-seekingalpha-react/index.js
+++ eslint-configs/eslint-config-seekingalpha-react/index.js
@@ -1,9 +1,6 @@
 module.exports = {
   extends: [
     // https://github.com/yannickcr/eslint-plugin-react
-    './rules/eslint/index.js',
-
-    // https://github.com/yannickcr/eslint-plugin-react
     './rules/eslint-plugin-react/index.js',
 
     // https://github.com/evcohen/eslint-plugin-jsx-a11y
@@ -14,7 +11,7 @@ module.exports = {
   ],
 
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 12,
     sourceType: 'module',
     ecmaFeatures: {
       impliedStrict: true,
