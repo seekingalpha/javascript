741d1e52512f7f3db30abb2c71894ba0ac644792
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2024-10-23 13:18:05 +0300

    5.38.65

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
-// eslint-disable-next-line node/no-unsupported-features/es-syntax
 import config from '@seekingalpha/prettier-config-samw';
 
-// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export, node/no-unsupported-features/es-syntax
+// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default {
   ...config,
 };
