bc27e75c4d6ae5d7da0610f1390027a5dc7fb7d3
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2025-03-06 17:34:05 +0200

    seekingalpha-javascript-style ver. 6.0.0 (#482)
    - migrate to flat config

--- .husky/install.js
+++ .husky/install.js
@@ -1,8 +1,9 @@
 try {
+  // eslint-disable-next-line import/dynamic-import-chunkname
   const husky = await import('husky');
   husky.default();
-} catch (e) {
-  if (e.code !== 'ERR_MODULE_NOT_FOUND') {
-    throw e;
+} catch (err) {
+  if (err.code !== 'ERR_MODULE_NOT_FOUND') {
+    throw err;
   }
 }
