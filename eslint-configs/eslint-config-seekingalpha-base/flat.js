c9ee70ee28c4dcfa470faecf0b026d9ebb1eb7e6
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2025-03-06 14:51:30 +0200

    eslint-config-seekingalpha-base ver. 8.17.1 (#479)
    - [breaking] removed separate prettier config

--- eslint-configs/eslint-config-seekingalpha-base/flat.js
+++ eslint-configs/eslint-config-seekingalpha-base/flat.js
@@ -1,4 +1,5 @@
-import esLintConfig from './rules/eslint/flat';
+// eslint-disable-next-line n/file-extension-in-import, import/extensions
+import esLintConfig from './rules/eslint/flat.js';
 
 // eslint-disable-next-line import/no-default-export, import/no-anonymous-default-export
 export default {
--- eslint-configs/eslint-config-seekingalpha-base/rules/eslint/flat.js
+++ eslint-configs/eslint-config-seekingalpha-base/rules/eslint/flat.js
@@ -1,6 +1,9 @@
-import layout from './layout-and-formatting';
-import problems from './possible-problems';
-import suggestions from './suggestions';
+// eslint-disable-next-line n/file-extension-in-import, import/extensions
+import layout from './layout-and-formatting.js';
+// eslint-disable-next-line n/file-extension-in-import, import/extensions
+import problems from './possible-problems.js';
+// eslint-disable-next-line n/file-extension-in-import, import/extensions
+import suggestions from './suggestions.js';
 
 // eslint-disable-next-line import/no-default-export, import/no-anonymous-default-export
 export default {
