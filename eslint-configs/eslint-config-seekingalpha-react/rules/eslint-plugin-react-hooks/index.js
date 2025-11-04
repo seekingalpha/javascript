d2d1037e01e44e2c0b520753d4f0bd15c7de0a89
NikSimonov <nikitasimonov09@gmail.com>
2025-11-04 14:39:38 +0200

    VER/9.63.0

--- eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react-hooks/index.js
+++ eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react-hooks/index.js
@@ -12,5 +12,22 @@ export default {
 
     // https://reactjs.org/docs/hooks-rules.html
     'react-hooks/exhaustive-deps': 'error',
+
+    // React Compiler rules
+    'react-hooks/config': 'error',
+    'react-hooks/error-boundaries': 'error',
+    'react-hooks/component-hook-factories': 'error',
+    'react-hooks/gating': 'error',
+    'react-hooks/globals': 'error',
+    'react-hooks/immutability': 'error',
+    'react-hooks/preserve-manual-memoization': 'error',
+    'react-hooks/purity': 'error',
+    'react-hooks/refs': 'error',
+    'react-hooks/set-state-in-effect': 'error',
+    'react-hooks/set-state-in-render': 'error',
+    'react-hooks/static-components': 'error',
+    'react-hooks/unsupported-syntax': 'warn',
+    'react-hooks/use-memo': 'error',
+    'react-hooks/incompatible-library': 'warn',
   },
 };
