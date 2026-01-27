5836482f261e2070f3ff0382056f7b224ee00fd9
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2026-01-27 10:58:05 +0200

    eslint-config-seekingalpha-base ver. 11.37.0 (#577)
    - [breaking] drop `eslint-plugin-array-func`

--- eslint-configs/eslint-config-seekingalpha-base/index.js
+++ eslint-configs/eslint-config-seekingalpha-base/index.js
@@ -1,19 +1,16 @@
 import esLintConfig from './rules/eslint/index.js';
-import esLintPluginArrayFuncConfig from './rules/eslint-plugin-array-func/index.js';
 import esLintPluginImport from './rules/eslint-plugin-import/index.js';
 import esLintPluginPromise from './rules/eslint-plugin-promise/index.js';
 import esLintPluginUnicorn from './rules/eslint-plugin-unicorn/index.js';
 
 export default {
   plugins: {
-    ...esLintPluginArrayFuncConfig.plugins,
     ...esLintPluginImport.plugins,
     ...esLintPluginPromise.plugins,
     ...esLintPluginUnicorn.plugins,
   },
   rules: {
     ...esLintConfig.rules,
-    ...esLintPluginArrayFuncConfig.rules,
     ...esLintPluginImport.rules,
     ...esLintPluginPromise.rules,
     ...esLintPluginUnicorn.rules,
