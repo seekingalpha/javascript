83983a6e7c3fbc3eac966846dcc2771e3e028b64
Aleksey Kovalevsky <aleksey@seekingalpha.com>
2026-01-30 20:07:25 +0200

    eslint-config-seekingalpha-base ver. 11.39.0 (#584)
    - [breaking] drop `eslint-plugin-promise` from oxlint config

--- eslint-configs/eslint-config-seekingalpha-base/oxc.js
+++ eslint-configs/eslint-config-seekingalpha-base/oxc.js
@@ -1,18 +1,28 @@
+import esLintConfig from './rules/eslint/index.js';
+import esLintPluginImport from './rules/eslint-plugin-import/index.js';
+import esLintPluginUnicorn from './rules/eslint-plugin-unicorn/index.js';
 import { ESLintDisabled } from './rules/eslint/oxlint-disabled.js';
 import { ESLintPluginPromiseDisabled } from './rules/eslint-plugin-promise/oxlint-disabled.js';
 import { ESLintPluginUnicornDisabled } from './rules/eslint-plugin-unicorn/oxlint-disabled.js';
 import { ESLintPluginImportDisabled } from './rules/eslint-plugin-import/oxlint-disabled.js';
 
-import config from './index.js';
-
 const oxcConfig = {
-  ...config,
+  plugins: {
+    ...esLintPluginImport.plugins,
+    ...esLintPluginUnicorn.plugins,
+  },
   rules: {
-    ...config.rules,
+    ...esLintConfig.rules,
+    ...esLintPluginImport.rules,
+    ...esLintPluginUnicorn.rules,
+    // Disabled rules for OXC projects
     ...ESLintDisabled,
     ...ESLintPluginImportDisabled,
     ...ESLintPluginPromiseDisabled,
     ...ESLintPluginUnicornDisabled,
+  },
+  settings: {
+    ...esLintPluginImport.settings,
   },
 };
 
