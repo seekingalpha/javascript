b9cd030242c732e9fc05e0553a162c3687f64334
NikSimonov <nikitasimonov09@gmail.com>
2026-03-17 18:22:51 +0200

    [TECH] fix oxlint react hooks config

--- eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react-hooks/oxlint-config.js
+++ eslint-configs/eslint-config-seekingalpha-react/rules/eslint-plugin-react-hooks/oxlint-config.js
@@ -1,7 +1,7 @@
 export default {
   plugins: ['react'],
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
+    'react/rules-of-hooks': 'error',
+    'react/exhaustive-deps': 'error',
   },
 };
